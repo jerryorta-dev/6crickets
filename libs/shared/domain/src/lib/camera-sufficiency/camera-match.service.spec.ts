d9ed89b657421809253efa01947fce2d6da60503
jerryorta-dev <jerryorta.dev@gmail.com>
2024-06-16 14:36:49 -0500

    test(domain): cameraMatchService

--- libs/shared/domain/src/lib/camera-sufficiency/camera-match.service.spec.ts
+++ libs/shared/domain/src/lib/camera-sufficiency/camera-match.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
+import { CameraRangeSpec, HardwareCamera } from './camera.model';
 
 import { CameraMatchService } from './camera-match.service';
+import { CAMERAS_AVAILABLE_LIST } from './cameras';
 
 describe('CameraMatchService', () => {
   let service: CameraMatchService;
@@ -10,7 +12,37 @@ describe('CameraMatchService', () => {
     service = TestBed.inject(CameraMatchService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should match cameras', () => {
+
+    const rangeSpec: CameraRangeSpec = {
+      distanceRange: [5, 8],
+      lightLevelRange: [540, 860]
+    }
+
+    service.matchCameras(rangeSpec);
+
+    const matchedCameras: HardwareCamera[] = [
+      CAMERAS_AVAILABLE_LIST[1],
+      CAMERAS_AVAILABLE_LIST[4],
+      CAMERAS_AVAILABLE_LIST[9],
+    ]
+
+    expect(service.isCameraMatched()).toBeTruthy();
+    expect(service.matchedCameras()).toEqual(matchedCameras);
+  });
+
+  it('should not match cameras', () => {
+
+    const rangeSpec: CameraRangeSpec = {
+      distanceRange: [10, 20],
+      lightLevelRange: [0, 50]
+    }
+
+    service.matchCameras(rangeSpec);
+
+    const matchedCameras: HardwareCamera[] = [];
+
+    expect(service.isCameraMatched()).toBeFalsy();
+    expect(service.matchedCameras()).toEqual(matchedCameras);
   });
 });
