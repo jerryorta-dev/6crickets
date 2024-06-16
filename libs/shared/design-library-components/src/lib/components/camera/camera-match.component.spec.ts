fef36addd7153f98d5f17ae84cf2b039b387ddd7
jerryorta-dev <jerryorta.dev@gmail.com>
2024-06-16 14:16:14 -0500

    test(dlc): camera-match
    unit tests

--- libs/shared/design-library-components/src/lib/components/camera/camera-match.component.spec.ts
+++ libs/shared/design-library-components/src/lib/components/camera/camera-match.component.spec.ts
@@ -15,7 +15,23 @@ describe('CameraMatchComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should initiate with default values', () => {
+    expect(component.lowerDistance).toBe(5);
+    expect(component.upperDistance).toBe(8);
+    expect(component.lowerLightLevel).toBe(540);
+    expect(component.upperLightLevel).toBe(860);
+  });
+
+  it('should emit rangeSpec on init', () => {
+    const emitSpy = jest.spyOn(component.selectedCameraRangeRange, 'emit');
+    component.ngOnInit();
+    expect(emitSpy).toHaveBeenCalledWith(component.rangeSpec);
+  });
+
+  it('should return CameraRangeSpec', () => {
+    expect(component.rangeSpec).toEqual({
+      distanceRange: [5, 8],
+      lightLevelRange: [540, 860]
+    });
   });
 });
